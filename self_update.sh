399f8ecf1ca6f29aa3780af5cc6506baf24d4208
Burasch <127677093+Burasch@users.noreply.github.com>
2026-04-26 15:52:43 +0200

    Modify self_update.sh to include itself in updates
    Updated the script to include self_update.sh in the checkout command while ignoring streams-ru.json.

--- self_update.sh
+++ self_update.sh
@@ -13,8 +13,8 @@ REMOTE=$(git rev-parse origin/main)
 if [ "$LOCAL" != "$REMOTE" ]; then
     echo "🆕 Update gefunden! Aktualisiere Code..."
     
-    # NUR Python Dateien und Docker-Files holen, streams-ru.json ignorieren
-    git checkout origin/main -- *.py Dockerfile docker-compose.yml streams-ru.json
+    # NUR Python Dateien und Docker-Files streams-ru.jsson, self_update.sh ,holen
+    git checkout origin/main -- *.py Dockerfile docker-compose.yml streams-ru.json self_update.sh
      
     # Den neuen Commit-Stand lokal markieren
     git merge origin/main --ff-only
